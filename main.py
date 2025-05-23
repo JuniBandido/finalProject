43f382cd94744e2a593ab07934786e1833aa5a75
José Noguera <juniornoguera1@gmail.com>
2025-05-23 15:44:30 -0600

    Registro y dinero al registrarse

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from app.AWSConnections import AWSConnections
 aws = AWSConnections()
 awsSession = aws.getSession()
 
-def saveUserDynamoDB(session, user):
-  dynamodb = session.resource('dynamodb')
-  table = dynamodb.Table('Users_firstTable')
-  response = table.put_item(Item=user)
-  return response
+def saveUserDynamoDB(session, user_data):
+    dynamodb = session.resource('dynamodb')
+    table = dynamodb.Table('Users_firstTable')
+    response = table.put_item(Item=user_data)
+    return response, print("Te has registrado con éxito! Recibiste $15,000")
 
-saveUserDynamoDB(awsSession, {"email": "local@local.com"})
-print("Proceso terminado")
+email = input("Ingresa tu correo electrónico: ")
+name = input("Ingrese su nombre: ")
+money = 15000
+
+user_item = {"email": email, "name": name, "money": money}
+
+saveUserDynamoDB(awsSession, user_item)
